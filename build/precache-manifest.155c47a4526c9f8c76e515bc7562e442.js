f94c7a4946b1468e700364420fc510b4ee1b4e75
Denny06 <denismacio@outlook.com>
2020-08-01 21:59:57 +0300

    deploy build

---
+++ build/precache-manifest.155c47a4526c9f8c76e515bc7562e442.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b3330585d95ec6ec92d2a45619f1a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc053236c7a0725a8d00",
+    "url": "/static/css/main.6c154ccf.chunk.css"
+  },
+  {
+    "revision": "38f7932f67aed9e0caa3",
+    "url": "/static/js/2.b8b36452.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b8b36452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc053236c7a0725a8d00",
+    "url": "/static/js/main.c7a26ab3.chunk.js"
+  },
+  {
+    "revision": "e6da1aaef45ddcd943f7",
+    "url": "/static/js/runtime-main.dff26a32.js"
+  },
+  {
+    "revision": "6c07f1de6a1f48ad61ed2c2e3b25a96e",
+    "url": "/static/media/h-bg.6c07f1de.jpg"
+  }
+]);
